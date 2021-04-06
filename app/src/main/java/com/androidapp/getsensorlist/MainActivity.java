efe12bc75fa37c69e4a4c6c141af0d0a387e6327
C-H Han <itq1@naver.com>
2021-04-06 10:19:32 +0900

    get sensor list

--- app/src/main/java/com/androidapp/getsensorlist/MainActivity.java
+++ app/src/main/java/com/androidapp/getsensorlist/MainActivity.java
@@ -2,13 +2,35 @@ package com.androidapp.getsensorlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+        TextView mTxtSensors;
+        SensorManager sensorMgr;
+        List<Sensor> sensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTxtSensors = findViewById(R.id.txtSensors);
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+        mTxtSensors.append("(# Sensors: " + sensorList.size() + ")\n\n");   //센서가 하나만 나오는게 아니고 계속 나오는거기 때문에 append씀
+
+        for(Sensor sensor : sensorList){
+            mTxtSensors.append("Sensor name: " + sensor.getName()+ "\n");
+            mTxtSensors.append("Sensor type: " + sensor.getType()+ "\n\n");
+        }
+        // 단순 정보만 조사하므로 onCreate에서 모든 작업 감당
     }
 }
